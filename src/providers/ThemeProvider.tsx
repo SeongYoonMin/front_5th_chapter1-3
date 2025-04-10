72ce31da38e8df3dd7f0777a1e088e98d37998ba
SeongYoon <namo4862@gmail.com>
2025-04-11 02:36:09 +0900

    fix: 컴포넌트 메모이제이션 추가

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,17 +1,17 @@
 import { useCallback, useState } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
-import { useMemo } from "../@lib";
+import { memo, useMemo } from "../@lib";
 import { Theme } from "../types";
 
-export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
-  const [theme, setTheme] = useState<Theme>("light");
-  const toggleTheme = useCallback(() => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-  }, []);
-  const value = useMemo(() => ({ theme, toggleTheme }), [theme, toggleTheme]);
-  return (
-    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
-  );
-};
+export const ThemeProvider: React.FC<{ children: React.ReactNode }> = memo(
+  ({ children }) => {
+    const [theme, setTheme] = useState<Theme>("light");
+    const toggleTheme = useCallback(() => {
+      setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    }, []);
+    const value = useMemo(() => ({ theme, toggleTheme }), [theme, toggleTheme]);
+    return (
+      <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+    );
+  },
+);
