a9864a2f97b95bb2228e60852f2110515f842110
SeongYoon <namo4862@gmail.com>
2025-04-11 02:54:31 +0900

    fix: Items가 자꾸 리렌더링 되는 현상 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { ThemeProvider } from "./providers/ThemeProvider";
 import { UserProvider } from "./providers/UserProvider";
 import { NotificationProvider } from "./providers/NotificationProvider";
@@ -7,9 +7,14 @@ import { ComplexForm } from "./components/ComplexForm";
 import { Header } from "./components/Header";
 import { ItemList } from "./components/ItemList";
 import { NotificationSystem } from "./components/NotificationSystem";
+import { generateItems } from "./utils";
 
 // 메인 App 컴포넌트
 const App: React.FC = () => {
+  const [items, setItems] = React.useState(generateItems(1000));
+  const addItems = useCallback(() => {
+    setItems((prev) => [...prev, ...generateItems(1000, prev.length)]);
+  }, []);
   return (
     <ThemeProvider>
       <NotificationProvider>
@@ -19,7 +24,7 @@ const App: React.FC = () => {
             <div className="container mx-auto px-4 py-8">
               <div className="flex flex-col md:flex-row">
                 <div className="w-full md:w-1/2 md:pr-4">
-                  <ItemList />
+                  <ItemList items={items} onAddItemsClick={addItems} />
                 </div>
                 <div className="w-full md:w-1/2 md:pl-4">
                   <ComplexForm />
