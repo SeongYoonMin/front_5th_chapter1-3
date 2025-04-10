bd42a61b14975c383de787f2847da1cfaf9a1b01
SeongYoon <namo4862@gmail.com>
2025-04-10 17:50:58 +0900

    fix: 얕은복사 개념 재정립 및 수정

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,45 +1,26 @@
-function isSameNull(objA: unknown, objB: unknown) {
-  return objA === objB;
-}
-
-function isSameUndefiend(objA: unknown, objB: unknown) {
-  return objA === objB;
-}
-
-function isSameArray<T>(arrA: T[], arrB: T[]) {
-  if (arrA.length !== arrB.length) return false;
-  for (let i = 0; i < arrA.length; i++) {
-    if (arrA[i] !== arrB[i]) return false;
-  }
-  return true;
-}
-
-function isSameObject<T extends Record<string, unknown>>(objA: T, objB: T) {
-  let result = false;
-  Object.keys(objA).forEach(([key]) => {
-    if (objA[key] === objB[key]) {
-      result = true;
-    } else {
-      result = false;
-    }
-  });
-  return result;
-}
+// 0410 문제점
+// 얕은비교가 똑바로 안이루어짐...
+// 얕은 비교에 대해 전혀 잘못 알고있었음.
+// 얕은 비교는 참조값만 비교하지, 내부 값을 비교하진않음.
+// 기존에 알던 개념은 얕은 복사만 하고 내부값까지 비교하는줄 알았음(얕은 비교니까..얕은복사비교인가? 했던 생각)
+// 얕은 비교는 단순히 참조값만 비교하는것.
 export function shallowEquals<T>(objA: T, objB: T): boolean {
+  // 같으면 true
   if (objA === objB) return true;
-  if (objA === null || objB === null) return isSameNull(objA, objB);
-  if (objA === undefined || objB === undefined)
-    return isSameUndefiend(objA, objB);
+
+  // null, undefined 체크
+  if (objA === null || objB === null) return false;
+  if (objA === undefined || objB === undefined) return false;
+
+  // 객체인지 아닌지 체크
+  if (typeof objA !== "object" || typeof objB !== "object") return false;
   // 배열일때
-  if (Array.isArray(objA) && Array.isArray(objB)) {
-    return isSameArray(objA as unknown[], objB as unknown[]);
-  }
-  // 객체일때
-  if (typeof objA === "object" && typeof objB === "object") {
-    return isSameObject(
-      objA as Record<string, unknown>,
-      objB as Record<string, unknown>,
-    );
-  }
-  return objA === objB;
+  if (Array.isArray(objA) !== Array.isArray(objB)) return false;
+
+  const keysA = Object.keys(objA) as (keyof T)[];
+  const keysB = Object.keys(objB) as (keyof T)[];
+
+  if (keysA.length !== keysB.length) return false;
+
+  return keysA.every((key) => Object.is(objA[key], objB[key]));
 }
